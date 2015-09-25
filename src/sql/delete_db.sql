e25207e6f2c3e4233607f1d714c128016a340153
Alexey <Alexey.N.Chernyshov@gmail.com>
2015-09-25 18:01:37 +0300

    bugfix delete_db.sql

--- src/sql/delete_db.sql
+++ src/sql/delete_db.sql
@@ -1,6 +1,6 @@
-DROP TABLE article_author;
+﻿DROP TABLE article_author;
 DROP TABLE cite;
+DROP TABLE article_keyword;
 DROP TABLE article;
 DROP TABLE author;
-DROP TABLE article_keyword;
 DROP TABLE keyword;
