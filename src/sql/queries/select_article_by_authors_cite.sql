e0d43af5b34f4fbee460adea2f9f63b38c1c7740
Alexey <Alexey.N.Chernyshov@gmail.com>
2015-09-30 09:09:38 +0300

    queries
    select articles by most cited authors
    select most cited articles
    select most cited authors

---
+++ src/sql/queries/select_article_by_authors_cite.sql
@@ -0,0 +1,22 @@
+﻿SELECT article.paper_title, SUM(cnt) AS auth_range
+FROM
+  article,
+  article_author,
+  (SELECT author.id, author.name, count(*) AS cnt
+    FROM
+      author,
+      article_author,
+      article,
+      cite
+    WHERE
+      author.id = article_author.author_id AND
+      article.id = article_author.article_id AND
+      article.id = cite.to_id
+    GROUP BY author.id
+  ) AS auth
+WHERE
+  article_author.author_id = auth.id AND
+  article.id = article_author.article_id 
+GROUP BY article.paper_title, article.id
+ORDER BY auth_range DESC
+  
